47b215e8bc0b02776e093e87cd96b61d0bfa5e37
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2024-12-24 11:45:01 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT ugb.TITLE,
+       ugb.BOARD_ID,
+       ugr.REPLY_ID,
+       ugr.WRITER_ID,
+       ugr.CONTENTS,
+       DATE_FORMAT(ugr.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+FROM USED_GOODS_BOARD as ugb
+INNER JOIN USED_GOODS_REPLY as ugr ON ugb.BOARD_ID = ugr.BOARD_ID
+WHERE ugb.CREATED_DATE LIKE '2022-10%'
+ORDER BY 6, 1
