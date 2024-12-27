e2d7487cffba116211041d74644718950a32ca34
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2024-12-27 09:22:08 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,12 +1,17 @@
 class Solution {
     public int solution(int n) {
         int sum = 1;
-        for(int i = 2; i <= n; i++) {
+        
+        if(n == 1 || n == 2) {
+            return n == 1 ? 1 : 2;
+        }
+        
+        for(int i = 2; i <= n / 2; i++) {
             if(n % i == 0) {
                 sum += i;
             }
         }
         
-        return n == 0 ? 0 : sum;
+        return n == 0 ? 0 : (sum + n);
     }
 }
