5895da63c4a4c5fa9b635e0798f54337e642f09e
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2024-12-31 12:40:27 +0900

    [Silver V] Title: K번째 수, Time: 1072 ms, Memory: 368404 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -5,23 +5,66 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] s = br.readLine().split(" ");
-        int n = Integer.parseInt(s[0]);
-        int k = Integer.parseInt(s[1]);
-        
-        String[] strArr = br.readLine().split(" ");
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(strArr[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        
-        Arrays.sort(arr);
-        
-        bw.write(String.valueOf(arr[k -1]));
-        bw.flush();
-        bw.close();
-        br.close();
+
+        quickSort(A, 0, N - 1, K - 1);
+        System.out.println(A[K - 1]);
+    }
+
+    public static void quickSort(int[] A, int start, int end, int K) {
+        if (start < end) {
+            int pivot = partition(A, start, end);
+            if (pivot == K) {
+                return;
+            } else if (K < pivot) {
+                quickSort(A, start, pivot - 1, K);
+            } else {
+                quickSort(A, pivot + 1, end, K);
+            }
+        }
+    }
+
+    public static int partition(int[] A, int start, int end) {
+        if(start + 1 == end) {
+            if(A[start] > A[end]) {
+                swap(A, start, end);
+                return end;
+            }
+        }
+
+        int middle = (start + end) / 2;
+        swap(A, start, middle);
+        int pivot = A[start];
+        int i = start + 1;
+        int j = end;
+
+        while(i <= j) {
+            while(i <= end && A[i] < pivot) {
+                i++;
+            }
+            while(j >= start + 1 && A[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(A, i++, j--);
+            }
+        }
+
+        A[start] = A[j];
+        A[j] = pivot;
+        return j;
+    }
+
+    public static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
