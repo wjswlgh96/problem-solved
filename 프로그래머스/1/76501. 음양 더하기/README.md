572e47d951397074db7b4cabf2afdf829d8a94c1
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-02 08:58:40 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 0.06 ms
+메모리: 75.7 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 31일 08:51:33
+2025년 01월 02일 08:58:35
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int result = 0;
+        int sum = 0;
         for(int i = 0; i < absolutes.length; i++) {
-            result += signs[i] ? absolutes[i] : (-1 * absolutes[i]);
+            sum += signs[i] ? absolutes[i] : (absolutes[i] * -1);
         }
         
-        return result;
+        return sum;
     }
 }
