def597ed2afa631a489f372117f43ff397943d89
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-06 13:15:53 +0900

    [level 2] Title: 타겟 넘버, Time: 4.59 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,37 +1,23 @@
 import java.util.ArrayList;
 
 class Solution {
-    static int n;
-    static ArrayList<Integer>[] A;
     static int count;
     
     public int solution(int[] numbers, int target) {
-        n = numbers.length;
-        A = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            A[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < n; i++) {
-            A[i].add(numbers[i]);
-            A[i].add(numbers[i] * -1);
-        }
-
-        DFS(A[0].get(0), 0, target);
-        DFS(A[0].get(1), 0, target);
-
+        count = 0;
+        DFS(numbers, 0, 0, target);
         return count;
     }
     
-    public static void DFS(int num, int depth, int target) {
-        if ((n - 1) == depth) {
-            if(num == target) {
+     public static void DFS(int[] numbers, int depth, int currentSum, int target) {
+        if (depth == numbers.length) {
+            if (currentSum == target) {
                 count++;
             }
             return;
         }
 
-        DFS((num + A[depth + 1].get(0)), depth + 1, target);
-        DFS((num + A[depth + 1].get(1)), depth + 1, target);
+        DFS(numbers, depth + 1, currentSum + numbers[depth], target);
+        DFS(numbers, depth + 1, currentSum - numbers[depth], target);
     }
 }
