ad94407b8b95b68319b18ce7e969642043072cfa
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-09 09:28:17 +0900

    [level 1] Title: 명예의 전당 (1), Time: 5.75 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,17 +4,13 @@ class Solution {
     public int[] solution(int k, int[] score) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         int result[] = new int[score.length];
-        queue.offer(score[0]);
-        result[0] = score[0];
-        for (int i = 1; i < score.length; i++) {
-            if (i < k) {
-                queue.offer(score[i]);
-                result[i] = queue.peek();
-            } else {
-                int max = Math.max(score[i], queue.poll());
-                queue.offer(max);
-                result[i] = queue.peek();
+        for (int i = 0; i < score.length; i++) {
+            queue.offer(score[i]);
+            if (queue.size() > k) {
+                queue.poll();
             }
+
+            result[i] = queue.peek();
         }
 
         return result;
