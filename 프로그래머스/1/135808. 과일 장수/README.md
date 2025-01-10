6db0ae11ee02eba27bc2ac507dcbc93583b4c042
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-10 09:45:35 +0900

    [level 1] Title: 과일 장수, Time: 45.68 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
 
 ### 성능 요약
 
-메모리: 145 MB, 시간: 277.37 ms
+메모리: 146 MB, 시간: 45.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 09:43:39
+2025년 01월 10일 09:45:32
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,21 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < score.length; i++) {
-            list.add(score[i]);
-        }
-        list.sort((a, b) -> b - a);
+        Arrays.sort(score);
 
         int result = 0;
-        for (int i = 0; i < list.size(); i = i + m) {
-            if ((i + m - 1) >= list.size()) {
-                break;
-            }
-
-            int min = Math.min(list.get(i), list.get(i + m - 1));
-            result += (min * m);
+        for (int i = score.length; i >= m; i -= m) {
+            result += score[i - m] * m;
         }
 
         return result;
