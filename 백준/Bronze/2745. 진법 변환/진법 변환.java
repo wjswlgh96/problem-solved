d1d90732c43f0d57920f83180625a631d1540447
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-13 10:08:15 +0900

    [Bronze II] Title: 진법 변환, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int result = Integer.parseInt(sc.next(), sc.nextInt());
+        System.out.println(result);
+    }
+
+}
