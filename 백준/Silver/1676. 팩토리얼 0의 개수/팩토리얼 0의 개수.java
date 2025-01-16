e61afd876092e3a24e0e30dcc57d42020d60a18d
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-16 10:06:25 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 176 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
@@ -8,21 +7,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        BigDecimal bd = new BigDecimal(1);
-        for (int i = 1; i <= n; i++) {
-            bd = bd.multiply(BigDecimal.valueOf(i));
-        }
-
-        String str = bd.toString();
         int count = 0;
-        for (int i = str.length() - 1; i >= 0; i--) {
-            if (str.charAt(i) == '0') {
-                count++;
-            } else {
-                break;
-            }
+        for(int i = 5; i <= n; i *= 5) {
+            count += n / i;
         }
-
+        
         System.out.println(count);
     }
 }
