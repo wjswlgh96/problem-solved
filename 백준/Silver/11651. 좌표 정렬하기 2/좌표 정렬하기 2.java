d7adb67199b840846e73318fe62e46410d8ae897
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-17 10:42:31 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 776 ms, Memory: 100788 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] arr, temp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(0, n - 1);
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void mergeSort(int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(left, mid);
+            mergeSort(mid + 1, right);
+
+            merge(left, mid, right);
+        }
+    }
+
+    private static void merge(int left, int mid, int right) {
+        temp = new int[right - left + 1][2];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i][1] < arr[j][1] || (arr[i][1] == arr[j][1] && arr[i][0] <= arr[j][0])) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int l = 0; l < temp.length; l++) {
+            arr[left + l] = temp[l];
+        }
+    }
+}
