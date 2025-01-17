9ed4eeb59bc3b2dd8683499807c86fe3ab36d689
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-17 09:20:50 +0900

    [level 1] Title: 숫자 짝꿍, Time: 96.08 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -2,38 +2,40 @@ import java.util.*;
 
 class Solution {
     public String solution(String X, String Y) {
-        Map<String, Integer> xMap = new HashMap();
-        Map<String, Integer> yMap = new HashMap();
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
 
         for (int i = 0; i < X.length(); i++) {
-            String s = String.valueOf(X.charAt(i));
-            xMap.put(s, xMap.getOrDefault(s, 0) + 1);
+            int idx = X.charAt(i) - '0';
+            xArr[idx]++;
         }
 
         for (int i = 0; i < Y.length(); i++) {
-            String s = String.valueOf(Y.charAt(i));
-            yMap.put(s, yMap.getOrDefault(s, 0) + 1);
+            int idx = Y.charAt(i) - '0';
+            yArr[idx]++;
         }
 
         StringBuilder result = new StringBuilder();
-        for (String s : xMap.keySet()) {
-            if (yMap.containsKey(s)) {
-                int min = Math.min(xMap.get(s), yMap.get(s));
-                result.append(s.repeat(min));
+        for (int i = 0; i < xArr.length; i++) {
+            int xValue = xArr[i];
+            int yValue = yArr[i];
+
+            if (xValue == 0 || yValue == 0) {
+                continue;
             }
+
+            int min = Math.min(xValue, yValue);
+            result.append(String.valueOf(i).repeat(min));
         }
-
-
-        char[] charArr = result.toString().toCharArray();
-        Arrays.sort(charArr);
-        result = new StringBuilder(new String(charArr)).reverse();
-
-        if (result.length() == 0) {
+        
+        String str = result.reverse().toString();
+        if (str.length() == 0) {
             return "-1";
-        } else if (result.charAt(0) == '0') {
+        } else if (str.charAt(0) == '0') {
             return "0";
         }
 
-        return result.toString();
+
+        return str;
     }
 }
