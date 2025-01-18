3df9a552f5bd2566a61ea897cc29b15bb0a6ed59
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-01-18 15:58:44 +0900

    [Gold IV] Title: 타임머신, Time: 260 ms, Memory: 19992 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Edge[] E = new Edge[m];
+        long[] distance = new long[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[1] = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            E[i] = new Edge(s, e, t);
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Edge edge = E[j];
+
+                if (distance[edge.start] != Integer.MAX_VALUE && distance[edge.end] > distance[edge.start] + edge.time) {
+                    distance[edge.end] = distance[edge.start] + edge.time;
+                }
+            }
+        }
+
+        boolean isCycle = false;
+        for (int i = 0; i < m; i++) {
+            Edge edge = E[i];
+
+            if (distance[edge.start] != Integer.MAX_VALUE && distance[edge.end] > distance[edge.start] + edge.time) {
+                 isCycle = true;
+            }
+        }
+
+        if (!isCycle) {
+            for (int i = 2; i <= n; i++) {
+                if (distance[i] == Integer.MAX_VALUE) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(distance[i] + "\n");
+                }
+            }
+        } else {
+            bw.write("-1");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static class Edge {
+        int start, end, time;
+
+        public Edge(int start, int end, int time) {
+            this.start = start;
+            this.end = end;
+            this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    ", time=" + time +
+                    '}';
+        }
+    }
+}
