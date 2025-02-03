3183dcddfb2b09e4af3d776ea7d5d8060f07daaa
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-02-03 09:10:59 +0900

    [level 2] Title: 피보나치 수, Time: 3.80 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 83.7 MB, 시간: 3.20 ms
+메모리: 89.4 MB, 시간: 3.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 13:52:38
+2025년 02월 03일 09:10:57
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int[] arr = new int[n + 1];
+        int[] arr = new int[100_001];
         arr[0] = 0;
         arr[1] = 1;
-        arr[2] = 1;
         
-        for(int i = 3; i <= n; i++) {
-            arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        for(int i = 2; i < 100_001; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
         }
         
         return arr[n];
