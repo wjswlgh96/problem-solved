0a41e6e6809f6b9fa44df2421ad98d7bd3d8f76b
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-02-04 09:14:51 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -7,8 +7,9 @@ class Solution {
             if(sum % i == 0) {
                 int width = sum / i;
                 if ((i - 2) * (width - 2) == yellow) {
-                    answer[0] = i;
-                    answer[1] = width;
+                    answer[0] = width;
+                    answer[1] = i;
+                    break;
                 }
             }
         }
