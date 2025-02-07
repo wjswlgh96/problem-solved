de924128fa7f17a0e691f780ee1e3285b7de2b26
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-02-07 09:53:59 +0900

    [level 1] Title: 신고 결과 받기, Time: 188.13 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,46 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> reportTargetMap = new HashMap<>();
-        Map<String, Integer> reportCntMap = new HashMap<>();
+        Map<String, Set<String>> reportTarget = new HashMap<>();
+        Map<String, Integer> reportCnt = new HashMap<>();
+
         for (String id : id_list) {
-            reportTargetMap.put(id, new HashSet<>());
-            reportCntMap.put(id, 0);
+            reportTarget.put(id, new HashSet<>());
+            reportCnt.put(id, 0);
         }
 
         for (String r : report) {
             String[] parts = r.split(" ");
             String reporter = parts[0];
             String target = parts[1];
-            
-            if (reportTargetMap.get(reporter).contains(target)) {
-                continue;
-            }
 
-            reportTargetMap.get(reporter).add(target);
-            reportCntMap.put(target, reportCntMap.get(target) + 1);
-        }
-
-        List<String> bannedUsers = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : reportCntMap.entrySet()) {
-            if (entry.getValue() >= k) {
-                bannedUsers.add(entry.getKey());
-            }
-        }
-
-        Map<String, Integer> reportResultMap = new HashMap<>();
-        for (Map.Entry<String, Set<String>> entry : reportTargetMap.entrySet()) {
-            for (String bannedUser : bannedUsers) {
-                if(entry.getValue().contains(bannedUser)) {
-                    reportResultMap.put(entry.getKey(), reportResultMap.getOrDefault(entry.getKey(), 0) + 1);
-                }
+            if (reportTarget.get(reporter).add(target)) {
+                reportCnt.put(target, reportCnt.get(target) + 1);
             }
         }
 
         int[] result = new int[id_list.length];
         for (int i = 0; i < id_list.length; i++) {
             String reporter = id_list[i];
-            result[i] = reportResultMap.getOrDefault(reporter, 0);
+            int count = 0;
+
+            for (String target : reportTarget.get(reporter)) {
+                if (reportCnt.get(target) >= k) {
+                    count++;
+                }
+            }
+
+            result[i] = count;
         }
 
         return result;
