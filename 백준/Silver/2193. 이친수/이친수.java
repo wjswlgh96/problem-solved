fddf5bfe48e150bea648ca2bb9ac2f7748b52c04
wjswlgh96 <59477979+wjswlgh96@users.noreply.github.com>
2025-02-11 10:47:44 +0900

    [Silver III] Title: 이친수, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,16 +1,16 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         long[][] D = new long[N + 1][2];
-        D[1][1] = 1;
         D[1][0] = 0;
+        D[1][1] = 1;
+
         for (int i = 2; i <= N; i++) {
-            D[i][0] = D[i - 1][1] + D[i - 1][0];
+            D[i][0] = D[i - 1][0] + D[i - 1][1];
             D[i][1] = D[i - 1][0];
         }
 
